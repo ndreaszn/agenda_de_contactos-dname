8f0acc60c9df2a9f9fc1952d57b9fd3a5de34c2d
Denise Andrea Name Ramirez <dnameramirez@gmail.com>
2025-09-26 10:01:29 +0200

    agenda menu finished

--- agenda/src/Main.java
+++ agenda/src/Main.java
@@ -1,13 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome :D"));
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome!");
+        System.out.println("1. Add contact");
+        System.out.println("2. Show contacts");
+        System.out.println("3. Search contact");
+        System.out.println("4. Exit");
+
+        System.out.print("Choose from 1-4: ");
+        int choice = scanner.nextInt();
+
+        System.out.print("Please wait a moment...");
     }
 }
